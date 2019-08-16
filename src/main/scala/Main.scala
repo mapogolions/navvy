2465fb1b3eb0935284aebb737cffd402e590ca75
mapogolions <ivanovpavelalex45@gmail.com>
2019-08-16 18:49:47 +0500

    Clean up entry point

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,32 +9,5 @@ import navvy.functor.FunctorSyntax._
 object Main {
   def main(args: Array[String]): Unit = {
     println("Hello world!")
-    //(digits.asInt | "1111loha" echo)
-    // ('a'.once >> 'b'.once >> 'c'.once).flatten | "abcad" echo
-    // tel | "3434-3434" echo
-   //  email | "ivanovgmail.com" echo
-
-    // ('-'.opt.once | "344-324234-34" echo)
-    // ('e'.opt >> 'e'.opt).many | "hello" echo
-    // ('e'.once.repeat(3) >> 'b'.once.repeat(2)).many | "eeebbeeebbeeebb" echo
-
-    // трюк с пропуском в середине
-    //('e'.once.repeat(2) <| whatever.repeat(2) >> 'l'.once  | "eeeelo" echo)
-    // пропустить первые три символа
-    // (whatever.repeat(3) |> 'a'.once | "---alloha" echo)
-
-    // порядковые селоекторы
-    // ('e'.once.after('b'.once) | "beahelo" echo)
-    // ('e'.once.before("hello".once) | "estopper" echo)
-    // ('e'.once.repeat(2).after('b'.atLeastOne) | "bbbeexponential" echo)
-
-    // Zero or one times
-    // ('e'.once.zeroOrOne | "" echo)
-
-    // пропускаем первые два символа
-    // (whatever >> whatever) |> 'b'.once | "aabus" echo
-    // 'e'.once.times(3)
-    // float | "-324.4545d" echo
-    (digit <|> letter <|> '_'.once <|> '$'.once).many | "helloer343woer342$34_jsdjfl" echo
   }
 }
